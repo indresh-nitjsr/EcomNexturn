dbb9a218f72a118ae6a5e8d547e19630841dd31b
Roma <romagupta701@gmail.com>
2023-07-05 19:13:38 +0530

    Registration page Added and user table has been created

--- CoreLogic/Services/UserService.cs
+++ CoreLogic/Services/UserService.cs
@@ -1,4 +1,5 @@
-﻿using CoreLogic.Models;
+﻿using CoreLogic.Data;
+using CoreLogic.Models;
 
 using System;
 using System.Collections.Generic;
@@ -10,14 +11,25 @@ namespace CoreLogic.Services;
 
 public class UserService
 {
+    MyContext ctx;
+    public UserService()
+    {
+        ctx = new MyContext();
+    }
+    public void AddUser(User user)
+    {
+        ctx.Users.Add(user);
+        ctx.SaveChanges();
+    }
     public List<User> GetAll()
     {
-        var users = new List<User>()
-        {
-            new User() { Id = 1,Name = "Sam" , City = "India"},
-            new User() { Id = 2,Name = "Ted" , City = "USA" }
-        };
-
-        return users;   
+       /* var users = new List<User>()
+         {
+            new User() { Id = 1,Name = "Sam" , City = "India" , Email = "sam@123" , Password="12345" , Phone= "6266784183"},
+            new User() { Id = 2,Name = "Ted" , City = "USA" , Email = "ted@123" , Password= "3456" , Phone="4679898668" }
+            
+         }; */
+        var users = ctx.Users.ToList();
+        return users;
     }
 }
